1b334d66eedc6051ca44b4302da03e8e03baa837
alieissa <you@example.com>
2018-06-26 21:47:08 -0400

    Implemented progressive pre-loading algorithm

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Image Pre-Loading
 
 This is a repo of code that I wrote and am wrtiting in exploring how various image pre-loading strategies can be implemented in Angular.
+
+
+#### Progresseive Pre-loading
+This is a pattern where a lower resolution image, hence smaller in size, is loaded initially and acts as a placeholder. When the higher resolution image is loaded the placeholder fades away. I believe this pattern was initially implemented in [Medium]('http://medium.com'). 
+
+Medium puts the placeholder in a ```Canvas``` and blurs it and hides the placeholder once the higher resolution image is loaded. I simply overlay the placeholder over the higher resolution image container and decrease gradually the placeholder image opacity to 0
--- src/app/__shared/components/hd-image/hd-image.component.ts
+++ src/app/__shared/components/hd-image/hd-image.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component,
+  OnInit,
+  Input,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'ls-hd-img',
@@ -8,12 +14,48 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HdImageComponent implements OnInit {
   @Input() src: any;
   @Input() placeholder: any;
+
+  @ViewChild('container') container: ElementRef;
   constructor() { }
 
   ngOnInit() {
-    console.log(this.src);
-    console.log(this.placeholder)
+    const loadImg = (placeholder) => {
+      const img = new Image();
+      img.src = this.src;
+      img.onload = () => {
+        /**
+        * This pushes the placeholder to background
+        * Note: A transition for the placeholder (.placeholder-img)
+        * is registered in style.css
+        */
+        placeholder.style.opacity = 0;
+        this.container.nativeElement.appendChild(img)
+      }
+    };
+
+    /**
+     * Since the placeholders are a scale of the desired image
+     * We use the placeholder aspect ration, the width of the containing
+     * element to calculate the desired height of the placeholder. The
+     * width is done automatically.
+     */
+    const imgPlaceholder = new Image();
+    imgPlaceholder.src = this.placeholder;
+    imgPlaceholder.onload = () => {
+      const aspectRatio = imgPlaceholder.height/imgPlaceholder.width;
+      const imgWidth = this.container.nativeElement.clientWidth;
+      const imgHeight = Math.floor(imgWidth * aspectRatio);
+
+
+      imgPlaceholder.width = imgWidth;  // Unable to do this with css
+      imgPlaceholder.className = 'placeholder-img';
+
+      // Set height to height of desired image
+      this.container.nativeElement.style.height = `${imgHeight}px`;
+      this.container.nativeElement.appendChild(imgPlaceholder);
+
+      loadImg(imgPlaceholder)
+    }
     
   }
-
 }

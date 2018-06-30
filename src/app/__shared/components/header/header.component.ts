f8b3383bce3b6fb5feef24c78f890fe1965868f3
alieissa <you@example.com>
2018-06-29 21:55:38 -0400

    Header clean up

--- src/app/__shared/components/header/header.component.ts
+++ src/app/__shared/components/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isMenuActive: boolean;
   constructor() { }
 
   ngOnInit() {
+    this.isMenuActive = false;
   }
 
 }
